cff98f62c33dae6448c10717a68ef5a3e449fa57
Linus <linus.jansson2000@gmail.com>
2020-05-09 21:37:26 +0200

    Added snake class pre, to later on clean up code and make it less confusing

---
+++ snakegame-cpp/snake.h
@@ -0,0 +1,19 @@
+#include "rect.h"
+/*
+
+Klassen orm kommer ärva från rektangel.
+och snake kommer bara ha ett object.
+
+
+
+*/
+namespace limpan
+{
+    class snake : rect
+    {
+        public:
+    
+        private:
+
+    };
+}    
