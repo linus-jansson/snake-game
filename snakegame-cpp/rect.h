4464686f90f918f09818d4c686345fcaec48bc05
Linus <linus.jansson2000@gmail.com>
2020-05-12 11:26:46 +0200

    Beautify

--- snakegame-cpp/rect.h
+++ snakegame-cpp/rect.h
@@ -9,20 +9,18 @@ namespace limpan
   {
   public:
     /* Include window when creating object so you can fetch rendrer*/
-    rect(limpan::window *win, int defaultxPos, int defaultyPos, int size)
+    rect(limpan::window *win, int defaultxPos, int defaultyPos, int width, int height, std::tuple<int, int, int, int> _rectColor)
         : _renderer(win->getRenderer()),
           _xPos(defaultxPos),
           _yPos(defaultyPos),
-          _size(size)
-    {
-    }
-    ~rect()
-    {
-    }
+          _width(width),
+          _height(height),
+          _RGBA(_rectColor) {}
+    ~rect() {}
 
-    void renderRect(std::tuple<int, int, int, int> &RGBA)
+    void renderRect(std::tuple<int, int, int, int> _RGBA)
     {
-      SDL_SetRenderDrawColor(_renderer, std::get<0>(RGBA), std::get<1>(RGBA), std::get<2>(RGBA), std::get<3>(RGBA));
+      SDL_SetRenderDrawColor(_renderer, std::get<0>(_RGBA), std::get<1>(_RGBA), std::get<2>(_RGBA), std::get<3>(_RGBA));
       SDL_RenderFillRect(_renderer, &_rect);
       SDL_RenderPresent(_renderer);
     }
@@ -33,7 +31,8 @@ namespace limpan
       _rect = {
           std::get<0>(_position) = x,
           std::get<1>(_position) = y,
-          w, h};
+          w, h
+        };
     }
 
     /* Set the position of the rectangle */
@@ -46,6 +45,9 @@ namespace limpan
     /* Return the current position of the rectangle */
     std::tuple<int, int> getPosition() { return _position; }
 
+  protected:
+  
+
   private:
     int _xPos = 0;
     int _yPos = 0;
@@ -53,12 +55,12 @@ namespace limpan
     int _width = 20;
     int _height = 20;
 
-    int _size;
+    std::tuple<int, int, int, int> _RGBA;
 
     std::tuple<int, int> _position = {_xPos, _yPos};
 
     SDL_Renderer *_renderer;
-    SDL_Rect _rect = {std::get<0>(_position), std::get<1>(_position), _size, _size};
+    SDL_Rect _rect = {std::get<0>(_position), std::get<1>(_position), _width, _height};
   };
 } // namespace limpan
 
